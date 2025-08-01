b8cc0603e6af49ec37f18e9ea42605680cb05642
imkrishn <963gurudev@gmail.com>
2025-08-01 17:13:12 +0530

    smartlink3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,15 +9,23 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  ...compat.extends(
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended"
+  ),
 
   {
     files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: require.resolve("@typescript-eslint/parser"),
+    },
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+    },
     rules: {
       "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/explicit-function-return-type": "warn",
     },
   },
 ];
-
-export default eslintConfig;
